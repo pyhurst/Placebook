36f93090619056f097fc08bdd43214d50ca3e456
blapete <pjblank2@gmail.com>
2020-05-29 13:21:21 -0400

    added controller route

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Dropdown from '../../components/Dropdown/Dropdown'
-import './Main.css'
+import React from "react";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import API from "../utils/API";
+import "./Main.css";
 
 const Main = () => {
-    return (
-        <div id="main">
-
-            <h1>this is our main page</h1>
-            <Dropdown />
-        </div>
-    )
-}
+  return (
+    <div id="main">
+      <h1>this is our main page</h1>
+      <Dropdown />
+    </div>
+  );
+};
 
 export default Main;
