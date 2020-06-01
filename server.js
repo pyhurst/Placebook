c28ebfe6ecc4e3b1814b895ab82789b7a7371662
Philip Hurst <youngphil9@gmail.com>
2020-06-01 06:38:34 -0500

    added routes, session, passport, morgan to server.js. Added connection.js and imported it to server.js

--- server.js
+++ server.js
@@ -1,24 +1,55 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('./passport');
+const mongoose = require('mongoose');
+const dbConnection = require('./connection');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+app.use(morgan('dev'));
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Mongoose DB Connection
+mongoose.Promise = global.Promise;
+
+// Sessions
+app.use(
+    session(
+        {
+            secret: 'facebook-special',
+            store: new MongoStore({ mongooseConnection: dbConnection }),
+            resave: false,
+            saveUninitialized: false
+        }
+    )
+)
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session()); 
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
 // Define API routes here
+const user = require('./routes/api/user');
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+app.use('/api/user', user);
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
