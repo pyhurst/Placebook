cdccb60febb77f93e7987854d7f28f82571ec5fd
blapete <pjblank2@gmail.com>
2020-06-01 14:25:36 -0400

    added businesspitch folder, businesshome, moved signup to component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import About from "./pages/About/About";
-import Business from "./pages/Business/Business";
-import Signup from "./pages/Signup/Signup";
+import Business from "./pages/BusinessPitch/Business";
+import Signup from "./components/Signup/Signup";
 import Login from "./pages/Login/Login";
 import { StoreProvider } from "./utils/UserContext";
-import Businesspage from "./pages/BusinessHome/BusinessHome";
+import Businesspage from "./pages/BusinessPage/BusinessPage";
 import Userpage from "./pages/UserHome/UserHome";
+import BusinessHome from "./pages/BusinessHome/BusinessHome";
 
 class App extends Component {
   render() {
@@ -22,7 +23,8 @@ class App extends Component {
             <Route exact path="/business" component={Business} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/business/home" component={Businesspage} />
+            <Route exact path="/business/page" component={Businesspage} />
+            <Route exact path="/business/home" component={BusinessHome} />
             <Route exact path="/user/home" component={Userpage} />
           </StoreProvider>
         </div>
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -13,11 +13,12 @@ const businessSeed = [
     phone: "312-555-5555",
     reservations: [],
     times: {
-        open: 0700,
-        close: 2100,
-        timeslot_length: 1.5,
-        capacity: 15
-    }
+      open: 0700,
+      close: 2100,
+      timeslot_length: 1.5,
+      capacity: 15,
+    },
+    ownerId: "5ed53422d58c3e451893c9b3",
   },
   {
     name: "Chicago Public Library",
@@ -26,11 +27,11 @@ const businessSeed = [
     phone: "312-777-3333",
     reservations: [],
     times: {
-        open: 0800,
-        close: 1700,
-        timeslot_length: 2,
-        capacity: 20
-    }
+      open: 0800,
+      close: 1700,
+      timeslot_length: 2,
+      capacity: 20,
+    },
   },
   {
     name: "Museum of Contemporary Art",
@@ -39,11 +40,11 @@ const businessSeed = [
     phone: "312-434-4343",
     reservations: [],
     times: {
-        open: 1000,
-        close: 2000,
-        timeslot_length: 2.5,
-        capacity: 75
-    }
+      open: 1000,
+      close: 2000,
+      timeslot_length: 2.5,
+      capacity: 75,
+    },
   },
   {
     name: "Pinstripes Bowling",
@@ -52,11 +53,11 @@ const businessSeed = [
     phone: "312-334-2222",
     reservations: 4,
     times: {
-        open: 1200,
-        close: 2300,
-        timeslot_length: 2.5,
-        capacity: 30
-    }
+      open: 1200,
+      close: 2300,
+      timeslot_length: 2.5,
+      capacity: 30,
+    },
   },
   {
     name: "CorePower Yoga",
@@ -65,11 +66,11 @@ const businessSeed = [
     phone: "312-334-2222",
     reservations: [],
     times: {
-        open: 0800,
-        close: 1900,
-        timeslot_length: 1,
-        capacity: 20
-    }
+      open: 0800,
+      close: 1900,
+      timeslot_length: 1,
+      capacity: 20,
+    },
   },
   {
     name: "Custom Business",
@@ -78,113 +79,124 @@ const businessSeed = [
     phone: "312-345-4564",
     reservations: [],
     times: {
-        open: 0800,
-        close: 1900,
-        timeslot_length: 1,
-        capacity: 10
-    }
-  }
+      open: 0800,
+      close: 1900,
+      timeslot_length: 1,
+      capacity: 10,
+    },
+  },
 ];
 
 const userSeed = [
-    {
-        username: "johnsmith",
-        password: "password1234",
-        email: "jsmith@gmail.com",
-        reservations: [{
-            business: "CorePower Yoga",
-            timeslot: 1600,
-            date: "2020-06-01"
-        }]
-    },
-    {
-        username: "janedoe",
-        password: "mypassword",
-        email: "jdoe1@gmail.com",
-        reservations: [{
-            business: "UFC Gym",
-            timeslot: 0900,
-            date: "2020-06-01"
-        }]
-    },
-    {
-        username: "emilyresch",
-        password: "emilyspass",
-        email: "eresch@gmail.com",
-        reservations: [{
-            business: "UFC Gym",
-            timeslot: 1100,
-            date: "2020-06-01"
-        }]
-    },
-    {
-        username: "philhurst",
-        password: "philspass",
-        email: "phil@gmail.com",
-        reservations: [{
-            business: "Museum of Contemporary Art",
-            timeslot: 1300,
-            date: "2020-06-03"
-        }]
-    },
-    {
-        username: "blapete",
-        password: "petespass",
-        email: "peter@yahoo.com",
-        reservations: [{
-            business: "Pinstripes Bowling",
-            timeslot: 2130,
-            date: "2020-06-01"
-        }]
-    },
-    {
-        username: "jimmyneutron",
-        password: "candybars",
-        email: "smartypants@gmail.com",
-        reservations: [{
-            business: "Chicago Public Library",
-            timeslot: 1000,
-            date: "2020-06-01"
-        }]
-    },
-    {
-        username: "spiderman",
-        password: "spiderpass",
-        email: "shootwebs@gmail.com",
-        reservations: [{
-            business: "CorePower Yoga",
-            timeslot: 0800,
-            date: "2020-06-02"
-        }]
-    },
-    {
-      username: "businessman",
-      password: "password",
-      email: "mybusiness@gmail.com"
-    }
-
+  {
+    username: "johnsmith",
+    password: "password1234",
+    email: "jsmith@gmail.com",
+    reservations: [
+      {
+        business: "CorePower Yoga",
+        timeslot: 1600,
+        date: "2020-06-01",
+      },
+    ],
+  },
+  {
+    username: "janedoe",
+    password: "mypassword",
+    email: "jdoe1@gmail.com",
+    reservations: [
+      {
+        business: "UFC Gym",
+        timeslot: 0900,
+        date: "2020-06-01",
+      },
+    ],
+  },
+  {
+    username: "emilyresch",
+    password: "emilyspass",
+    email: "eresch@gmail.com",
+    reservations: [
+      {
+        business: "UFC Gym",
+        timeslot: 1100,
+        date: "2020-06-01",
+      },
+    ],
+  },
+  {
+    username: "philhurst",
+    password: "philspass",
+    email: "phil@gmail.com",
+    reservations: [
+      {
+        business: "Museum of Contemporary Art",
+        timeslot: 1300,
+        date: "2020-06-03",
+      },
+    ],
+  },
+  {
+    username: "blapete",
+    password: "petespass",
+    email: "peter@yahoo.com",
+    reservations: [
+      {
+        business: "Pinstripes Bowling",
+        timeslot: 2130,
+        date: "2020-06-01",
+      },
+    ],
+  },
+  {
+    username: "jimmyneutron",
+    password: "candybars",
+    email: "smartypants@gmail.com",
+    reservations: [
+      {
+        business: "Chicago Public Library",
+        timeslot: 1000,
+        date: "2020-06-01",
+      },
+    ],
+  },
+  {
+    username: "spiderman",
+    password: "spiderpass",
+    email: "shootwebs@gmail.com",
+    reservations: [
+      {
+        business: "CorePower Yoga",
+        timeslot: 0800,
+        date: "2020-06-02",
+      },
+    ],
+  },
+  {
+    username: "businessman",
+    password: "password",
+    email: "mybusiness@gmail.com",
+  },
 ];
 
 db.Business.remove({})
   .then(() => db.Business.collection.insertMany(businessSeed))
-  .then(data => {
+  .then((data) => {
     console.log(data.result.n + " business records inserted!");
     process.exit(0);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
     process.exit(1);
   });
 
-db.User.remove({})
-  .then(() => db.User.collection.insertMany(userSeed))
-  .then(data => {
-    console.log(data.result.n + " user records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
-
-
+// db.User.remove({})
+//   .then(() => db.User.collection.insertMany(userSeed))
+//   .then((data) => {
+//     console.log(data.result.n + " user records inserted!");
+//     process.exit(0);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     process.exit(1);
+//   });
