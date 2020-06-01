05f7a21a9c3d8381f70548fde3c4f7bb528ad9e7
Philip Hurst <youngphil9@gmail.com>
2020-06-01 06:16:11 -0500

    added user.js to api routes, post route on api/signup

---
+++ routes/api/user.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/user');
+const passport = require('../../passport');
+
+router.post('/api/signup', (req, res) => {
+    console.log('user signup');
+
+    const { username, password, email } = req.body;
+    console.log(username, password, email);
+
+    // Validation
+    User.findOne({ username: username }, (err, user) => {
+        if (err) {
+            console.log('User.js post error: ', err)
+        } else if (user) {
+            res.json({
+                error: `Sorry, already a user with the username: ${username}`
+            })
+        } else {
+            const newUser = new User(
+                {
+                    username: username,
+                    password: password,
+                    email: email
+                }
+            )
+            newUser.save((err, savedUser) => {
+                if (err) return res.json(err);
+                res.json(savedUser)
+            })
+        }
+    });
+});
