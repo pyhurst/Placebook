be105f127ebd3d10f0fbb3a397f38b7fe8a6385e
Philip Hurst <youngphil9@gmail.com>
2020-06-01 05:23:07 -0500

    added unique parameter to model with checkPassword and hashPassword methods, import bcrypt

--- models/user.js
+++ models/user.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+mongoose.promise = Promise;
 
 const userSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    email: String,
-    reservations: { type: Array }
-  });
-  
-  const User = mongoose.model("User", userSchema);
-  
-  module.exports = User;
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, unique: false },
+  email: { type: String, required: true, unique: true },
+  reservations: { type: Array }
+});
+
+userSchema.methods = {
+  checkPassword: function (inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.password)
+  },
+  hashPassword: plainTextPassword => {
+    return bcrypt.hashSync(plainTextPassword, 10)
+  }
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
