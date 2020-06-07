e251bfe7eeb9995023f668d8d25f18e5769d046e
Philip Hurst <youngphil9@gmail.com>
2020-06-07 11:10:08 -0500

    user can cancel a meeting and deletes from user array in mongo

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,12 +22,33 @@ module.exports = {
             .then((dbModel) => res.json(dbModel))
             .catch((err) => res.status(422).json(err));
     },
-    //   deleteUserReservation: function (req, res) {
-    //     db.User.update(
-    //       { _id: req.body.userid },
-    //       {
-    //         $pull: { reservations: { businessId: req.body.businessId } },
-    //       }
-    //     );
-    //   },
+    deleteUserReservation: function (req, res) {
+        db.User.findById({ _id: req.params.id })
+            .then((userModel) => {
+                console.log(userModel);
+                const filteredOut = [];
+                console.log('delete reservatoin')
+                console.log(userModel.reservations)
+                // console.log(req.body.customerIds[0])
+                for (let i = 0; i < userModel.reservations.length; i++) {
+                    if (userModel.reservations[i]._id !== req.body.resId) {
+                        console.log(userModel.reservations[i]);
+                        filteredOut.push(userModel.reservations[i]);
+                    }
+                }
+                // res.json(filteredOut);
+                db.User.findByIdAndUpdate(
+                    { _id: req.params.id },
+                    {
+                        $set: {
+                            reservations: filteredOut,
+                        },
+                    },
+                    { new: true }
+                )
+                    .then((dbModel) => res.json(dbModel))
+                    .catch((err) => res.status(422).json(err));
+            })
+            .catch((err) => console.log(err));
+    },
 };
