d7a09558cd0102dfb7aeef6194f04b8e8cacad54
Philip Hurst <youngphil9@gmail.com>
2020-06-07 09:46:17 -0500

    when user reservation saves to user reservations array in mongo, a random id is assigned

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,33 @@
 const db = require("../models");
+const shortid = require('shortid');
 
 module.exports = {
-  addUserReservation: function (req, res) {
-    db.User.findByIdAndUpdate(
-      { _id: req.params.id },
-      {
-        $push: {
-          reservations: req.body,
-        },
-      },
-      { new: true }
-    )
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
-  },
-  //   deleteUserReservation: function (req, res) {
-  //     db.User.update(
-  //       { _id: req.body.userid },
-  //       {
-  //         $pull: { reservations: { businessId: req.body.businessId } },
-  //       }
-  //     );
-  //   },
+    addUserReservation: function (req, res) {
+        const obj = {
+            _id: shortid.generate(),
+            date: req.body.date,
+            time: req.body.time,
+            businessId: req.body.businessId,
+            businessName: req.body.businessName
+        };
+        db.User.findByIdAndUpdate(
+            { _id: req.params.id },
+            {
+                $push: {
+                    reservations: obj,
+                },
+            },
+            { new: true }
+        )
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    },
+    //   deleteUserReservation: function (req, res) {
+    //     db.User.update(
+    //       { _id: req.body.userid },
+    //       {
+    //         $pull: { reservations: { businessId: req.body.businessId } },
+    //       }
+    //     );
+    //   },
 };
