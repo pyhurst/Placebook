99e0e0452fad233de468853b57d7391110140473
Philip Hurst <youngphil9@gmail.com>
2020-06-19 08:13:42 -0500

    cards are all the same sizes now, took link of view business page out and added it to the entire card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -13,9 +13,11 @@ const Card = (props) => {
   return (
     <div>
       <MDBCol>
-        <MDBCard style={{ width: "15rem" }}>
+        <MDBCard style={{ width: "15rem" , "height": "32rem"}}>
+          <Link to={"/business/page/" + props._id}>
           <MDBCardImage
             className="img-fluid"
+            style={{"height": "15rem"}}
             src={props.image}
             waves
           />
@@ -23,8 +25,9 @@ const Card = (props) => {
             <MDBCardTitle style={{"color": "rgb(120, 200, 166)"}} >{props.name}</MDBCardTitle>
             <MDBCardText>{props.category}</MDBCardText>
             <MDBCardText>{props.address}</MDBCardText>
-            <Link to={"/business/page/" + props._id} style={{"color": "rgb(100, 180, 214)"}}>View Business Page</Link>
+            {/* <Link to={"/business/page/" + props._id} style={{"color": "rgb(100, 180, 214)"}}>View Business Page</Link> */}
           </MDBCardBody>
+          </Link>
         </MDBCard>
       </MDBCol>
     </div>
